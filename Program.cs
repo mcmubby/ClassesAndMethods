3564255cbed70ba5a586d66e6cc6b215605d4e6c
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-04-28 12:56:36 +0100

    end of class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,64 @@ namespace ClassesAndMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // student sam = new student();
+            // student john = new student();
+            // sam.studentDetails(312, 012345,"Sam","Allardyce", "somewhere in Sambisa");
+            // john.studentDetails(382, 678910, "John","Terry", "somewhere in another forest");
+
+            // student sam = new student(312, 012345,"Sam","Allardyce", "somewhere in Sambisa");
+            // student john = new student(382, 678910, "John","Terry", "somewhere in another forest");
+
+            // ekene.addNumbers(10,30);
+
+            equilateralTriangle test = new equilateralTriangle(5,3);
+            test.Area();
+            test.Perimeter();
+        }
+    }
+
+    public class student
+    {
+        // public student(int rollNumber, int phoneNumber, string firstName, string lastName, string homeAddress)
+        // {
+        //     Console.WriteLine($"Student name : {firstName} {lastName} \nRoll number : {rollNumber}\n");
+        // }
+               
+        // public void addNumbers(int firstNumber, int secondNumber)
+        // {
+        //     int result = firstNumber + secondNumber;
+        //     Console.WriteLine(result);
+        // }
+
+        public void studentDetails(int rollNumber, int phoneNumber, string firstName, string lastName, string homeAddress)
+        {
+            Console.WriteLine($"Student name : {firstName} {lastName} \nRoll number : {rollNumber} \nPhone number : {phoneNumber} \nAddress : {homeAddress}\n");
+        }
+    }
+
+
+    public class equilateralTriangle
+    {
+        double _baseLength;
+        double _height;
+ 
+        public equilateralTriangle(double baseLenght, double height)
+        {
+            _baseLength = baseLenght;
+            _height = height;
+            
+        }
+
+        public void Area()
+        {
+            double result = (0.5 * _baseLength) * _height;
+            Console.WriteLine($"The area of the triangle is {result}");
+        }
+
+        public void Perimeter()
+        {
+            double result = 3*(_baseLength);
+            Console.WriteLine($"The perimeter of the equilateral triangle is {result}");
         }
     }
 }
